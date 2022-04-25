c1a8770a26e3523c4027debc3560f87c280000a8
JeongMyeongHong <hkdhjm@gmail.com>
2022-04-25 12:07:56 +0900

    12:00 recommit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import {Layout} from "./common"
+import {wrapper} from '../redux/store.ts'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp)
